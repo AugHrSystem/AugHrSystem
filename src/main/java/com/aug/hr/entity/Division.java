9b844fba8e6a544028f98188c4aaf5b3f25eaa12
PreeyaMay <leungsod@hotmail.com>
2015-04-21 09:39:44 +0700

    spring jdbc entity

---
+++ src/main/java/com/aug/hr/entity/Division.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Preeyaporn
+ * @date 21 เม.ย. 2558
+ */
+package com.aug.hr.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Table(name = "Division")
+@Repository
+public class Division {
+	
+	
+
+}
---
+++ src/main/java/com/aug/hr/entity/Education.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Preeyaporn
+ * @date 21 เม.ย. 2558
+ */
+package com.aug.hr.entity;
+
+public class Education {
+
+}
