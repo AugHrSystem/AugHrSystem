5e9caf00c8649b4fed890f117e921a58251ad69f
apivakim <nuva_apiva@outlook.co.th>
2015-04-21 17:48:00 +0700

    EmpFamily and MasTecnology

---
+++ src/main/java/com/aug/hr/entity/MasTechnology.java
@@ -0,0 +1,47 @@
+package com.aug.hr.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Table(name="MASTECHNOLOGY")
+@Repository
+public class MasTechnology extends BaseEntity implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9016889793391444496L;
+	private Integer masTechnologyId;
+	private String masTechnologyName;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	public Integer getMasTechnologyId() {
+		return masTechnologyId;
+	}
+	
+	public void setMasTechnologyId(Integer masTechnologyId) {
+		this.masTechnologyId = masTechnologyId;
+	}
+	
+	@Column(name="NAME",nullable=false)
+	public String getMasTechnologyName() {
+		return masTechnologyName;
+	}
+	
+	public void setMasTechnologyName(String masTechnologyName) {
+		this.masTechnologyName = masTechnologyName;
+	}
+	
+	
+}
