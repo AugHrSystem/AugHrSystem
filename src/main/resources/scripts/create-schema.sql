73a7f934006d0ea2a08eb3bad7273a6b45964a34
amanurat <assanai17@gmail.com>
2015-04-21 15:54:43 +0700

    create script to add user

--- src/main/resources/scripts/create-schema.sql
+++ src/main/resources/scripts/create-schema.sql
@@ -1,21 +1,49 @@
 
+    alter table CORESKILL 
+        drop 
+        foreign key FK489815F2C3FB1C25;
+
     alter table EDUCATION 
         drop 
         foreign key FK94C37788264515AF;
 
     alter table EMPLOYEE 
         drop 
-        foreign key FK75C8D6AEA93422D5;
+        foreign key FK75C8D6AE9137778B;
 
     alter table EMPLOYEE 
         drop 
-        foreign key FK75C8D6AE9137778B;
+        foreign key FK75C8D6AECFD0D4C9;
+
+    alter table EMPLOYEE 
+        drop 
+        foreign key FK75C8D6AE56F7F72B;
+
+    alter table EXPERIENCE 
+        drop 
+        foreign key FK17D0456AEFB7E9CD;
+
+    drop table if exists CORESKILL;
 
     drop table if exists EDUCATION;
 
     drop table if exists EMPLOYEE;
 
-    drop table if exists coreSkill;
+    drop table if exists EXPERIENCE;
+
+    create table CORESKILL (
+        ID integer not null auto_increment,
+        auditFlag varchar(255),
+        createdBy integer,
+        createdTimeStamp datetime,
+        updatedBy integer,
+        updatedTimeStamp datetime,
+        CODE varchar(255),
+        ISACTIVE bit,
+        NAME varchar(255),
+        CORESKILL_ID bigint,
+        primary key (ID)
+    );
 
     create table EDUCATION (
         ID integer not null auto_increment,
@@ -36,23 +64,36 @@
     create table EMPLOYEE (
         ID bigint not null auto_increment,
         NAME varchar(255),
-        coreSkill integer,
+        CORESKILL_ID integer,
         EDUCATION_ID integer,
+        EXPERIENCE_ID integer,
         primary key (ID)
     );
 
-    create table coreSkill (
+    create table EXPERIENCE (
         ID integer not null auto_increment,
         auditFlag varchar(255),
         createdBy integer,
         createdTimeStamp datetime,
         updatedBy integer,
         updatedTimeStamp datetime,
-        CODE varchar(255),
-        isActive bit,
-        NAME varchar(255),
+        ADDRESS varchar(255),
+        BUSINESS varchar(255),
+        COMPANY_NAME varchar(255),
+        DATE_FROM datetime,
+        DATE_TO datetime,
+        DESCRIPTION varchar(255),
+        POSITION varchar(255),
+        SUPERVISOR varchar(255),
+        EXPERIENCE_ID bigint,
         primary key (ID)
     );
+
+    alter table CORESKILL 
+        add index FK489815F2C3FB1C25 (CORESKILL_ID), 
+        add constraint FK489815F2C3FB1C25 
+        foreign key (CORESKILL_ID) 
+        references EMPLOYEE (ID);
 
     alter table EDUCATION 
         add index FK94C37788264515AF (EMP_ID), 
@@ -61,13 +102,25 @@
         references EMPLOYEE (ID);
 
     alter table EMPLOYEE 
-        add index FK75C8D6AEA93422D5 (coreSkill), 
-        add constraint FK75C8D6AEA93422D5 
-        foreign key (coreSkill) 
-        references coreSkill (ID);
-
-    alter table EMPLOYEE 
         add index FK75C8D6AE9137778B (EDUCATION_ID), 
         add constraint FK75C8D6AE9137778B 
         foreign key (EDUCATION_ID) 
         references EDUCATION (ID);
+
+    alter table EMPLOYEE 
+        add index FK75C8D6AECFD0D4C9 (EXPERIENCE_ID), 
+        add constraint FK75C8D6AECFD0D4C9 
+        foreign key (EXPERIENCE_ID) 
+        references EXPERIENCE (ID);
+
+    alter table EMPLOYEE 
+        add index FK75C8D6AE56F7F72B (CORESKILL_ID), 
+        add constraint FK75C8D6AE56F7F72B 
+        foreign key (CORESKILL_ID) 
+        references CORESKILL (ID);
+
+    alter table EXPERIENCE 
+        add index FK17D0456AEFB7E9CD (EXPERIENCE_ID), 
+        add constraint FK17D0456AEFB7E9CD 
+        foreign key (EXPERIENCE_ID) 
+        references EMPLOYEE (ID);
---
+++ src/main/resources/scripts/setup-user.sql
@@ -0,0 +1,4 @@
+
+GRANT ALL ON hris_system.* TO 'aug@localhost' IDENTIFIED BY 'Aug@min';
+GRANT ALL ON hris_system.* TO 'aug@127.0.0.1' IDENTIFIED BY 'Aug@min';
+
