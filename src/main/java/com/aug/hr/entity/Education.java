e7224099e56199fd17002e67b7dac0d22e221bd1
PreeyaMay <leungsod@hotmail.com>
2015-04-21 10:11:13 +0700

    EDU AND DIV

--- src/main/java/com/aug/hr/entity/Education.java
+++ src/main/java/com/aug/hr/entity/Education.java
@@ -5,6 +5,101 @@
  */
 package com.aug.hr.entity;
 
-public class Education {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Table(name = "Education")
+@Repository
+public class Education extends BaseEntity{
+	
+	@Id
+	@Column(name = "EDU_ID")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name = "EDU_UNIVERSITY" ,nullable = false)
+	private String university;
+	
+	@Column(name = "EDU_GPA" ,nullable =false)
+	private String gpa;
+	
+	@Column(name = "EDU_FACULTY" ,nullable =false)
+	private String faculty;
+	
+	@Column(name = "EDU_MAJOR" ,nullable =false)
+	private String major;
+	
+	@Column(name = "EDU_DEGREE" ,nullable =false)
+	private String degree;
+	
+	@ManyToOne
+	@JoinColumn(name = "EMP_ID" ,referencedColumnName="id")
+	private Employee employee;
+
+	/*---------------------- getter / setter ----------------------*/
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(String gpa) {
+		this.gpa = gpa;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
 }
