6405f455605e96d236e9b6200e96257e840cc886
bill <bisbifltl@gmail.com>
2015-04-27 10:43:26 +0700

    add dao daoimpl service

---
+++ src/main/java/com/aug/hr/dao/AddressTypeDao.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author natechanok
+ * @date Apr 27, 2015
+ */
+
+package com.aug.hr.dao;
+
+import com.aug.hr.entity.AddressType;
+
+public interface AddressTypeDao extends GenericDao<AddressType, Integer>{
+
+}
