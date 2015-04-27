06332667f8f67a13075bbdaacf4ad4407f0e86b8
PreeyaMay <leungsod@hotmail.com>
2015-04-27 10:25:44 +0700

    add DivDao,DivDaoImpl,EduService,EduDao,EduDaoImpl

---
+++ src/main/java/com/aug/hr/dao/DivisionDao.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Preeyaporn
+ * @date 27 เม.ย. 2558
+ */
+package com.aug.hr.dao;
+
+import com.aug.hr.entity.Division;
+
+public interface DivisionDao extends GenericDao<Division, Integer>{
+
+}
