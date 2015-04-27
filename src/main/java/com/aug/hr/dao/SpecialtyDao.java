fb67b64f8ef291d91ff8a9e4e4c93c8de5594e3f
Thanlak <paopeed.kik@gmail.com>
2015-04-27 10:53:44 +0700

    service dao reference and specialty

---
+++ src/main/java/com/aug/hr/dao/SpecialtyDao.java
@@ -0,0 +1,15 @@
+package com.aug.hr.dao;
+
+import java.util.List;
+
+import com.aug.hr.entity.Specialty;
+import com.aug.hr.services.Reference;
+
+
+
+public interface SpecialtyDao extends GenericDao<Specialty, Integer>{
+	
+	public List<SpecialtyDao>findByCriteria(SpecialtyDao specialty);
+	public SpecialtyDao delete(Integer id);
+
+}
