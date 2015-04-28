d2037dd44115ab9c88097569c5b4844aff0ce00e
Parnell <takoyakimuffin@gmail.com>
2015-04-28 11:42:54 +0700

    Add test service

--- src/test/java/dao/CoreSkillDaoTest.java
+++ src/test/java/dao/CoreSkillDaoTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,20 +24,21 @@ public class CoreSkillDaoTest {
 	@Autowired
 	private CoreSkillDao coreSkillDao;
 	
-//	@Test
-//	public void createCoreSkill(){
-//		Calendar calendar = Calendar.getInstance();
-//		CoreSkill coreSkill = new CoreSkill();
-//		coreSkill.setAuditFlag("C");
-//		coreSkill.setCode("PR");
-//		coreSkill.setCreatedBy(001);
-//		coreSkill.setCreatedTimeStamp(calendar.getTime());
-//		coreSkill.setId(1);
-//		coreSkill.setIsActive(true);
-//		coreSkill.setName("Programmer");
-//		coreSkillDao.getCurrentSession().save(coreSkill);		
-//	}
-//	
+	@Test
+	@Rollback(false)
+	public void createCoreSkill(){
+		Calendar calendar = Calendar.getInstance();
+		CoreSkill coreSkill = new CoreSkill();
+		coreSkill.setAuditFlag("C");
+		coreSkill.setCode("PR");
+		coreSkill.setCreatedBy(001);
+		coreSkill.setCreatedTimeStamp(calendar.getTime());
+		coreSkill.setId(1);
+		coreSkill.setIsActive(true);
+		coreSkill.setName("Programmer");
+		coreSkillDao.getCurrentSession().save(coreSkill);		
+	}
+	
 //	@Test
 //	public void updateCoreSkill(){
 //		CoreSkill coreSkill = (CoreSkill) coreSkillDao.getCurrentSession().get(CoreSkill.class, 1);
@@ -56,12 +58,12 @@ public class CoreSkillDaoTest {
 //		int id = coreSkill.getId();
 //		Assert.assertEquals(1, id);
 //	}
-	
-	@Test
-	public void findAllCoreSkill(){
-		Criteria c = coreSkillDao.getCurrentSession().createCriteria(CoreSkill.class);
-		List<CoreSkill> coreSkills = c.list();
-		Assert.assertEquals(19, coreSkills.size());
-	}
+//	
+//	@Test
+//	public void findAllCoreSkill(){
+//		Criteria c = coreSkillDao.getCurrentSession().createCriteria(CoreSkill.class);
+//		List<CoreSkill> coreSkills = c.list();
+//		Assert.assertEquals(19, coreSkills.size());
+//	}
 
 }
