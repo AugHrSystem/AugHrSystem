2211b8521f8ff527ebf6f1f28c2041ddf166099c
Pranrajit <pranrajit@users.noreply.github.com>
2015-05-07 12:00:37 +0700

    add @JSONIGNORE

--- src/main/java/com/aug/hr/entity/MasSpecialty.java
+++ src/main/java/com/aug/hr/entity/MasSpecialty.java
@@ -14,6 +14,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 
 @Entity
@@ -37,7 +39,7 @@ public class MasSpecialty extends BaseEntity{
 	
 	
 	
-	
+	@JsonIgnore
 	@OneToMany(mappedBy = "masspecialty")
 	private Set<Ability> abilities;
 
