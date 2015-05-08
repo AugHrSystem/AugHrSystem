8440cd09c7d026c100a5ab968c3ad39f249f8b0c
Pranrajit <pranrajit@users.noreply.github.com>
2015-05-08 15:18:35 +0700

    MasCoreskillService and MasCoreskiiServiceImpl

--- src/main/java/com/aug/hr/dao/MasCoreSkillDao.java
+++ src/main/java/com/aug/hr/dao/MasCoreSkillDao.java
@@ -1,7 +1,13 @@
 package com.aug.hr.dao;
 
+import java.util.List;
+
 import com.aug.hr.entity.MasCoreSkill;
 
 public interface MasCoreSkillDao extends GenericDao<MasCoreSkill, Integer> {
+
+	List<MasCoreSkill> findByCriteria(MasCoreSkill masCoreSkill);
+
+	MasCoreSkill deleteById(Integer id);
 
 }
---
+++ src/main/java/com/aug/hr/services/impl/MasCoreSkillServiceImpl.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * @author Pranrajit
+ * @date 8 พ.ค. 2558
+ */
+package com.aug.hr.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aug.hr.dao.MasCoreSkillDao;
+import com.aug.hr.entity.MasCoreSkill;
+import com.aug.hr.services.MasCoreSkillService;
+
+public class MasCoreSkillServiceImpl implements MasCoreSkillService{
+
+	
+	
+	@Autowired
+	private MasCoreSkillDao masCoreSkillDao;
+	
+	@Override
+	public void create(MasCoreSkill masCoreSkill) {
+		masCoreSkillDao.create(masCoreSkill);
+		
+	}
+
+	@Override
+	public void update(MasCoreSkill masCoreSkill) {
+		masCoreSkillDao.update(masCoreSkill);
+		
+	}
+
+	@Override
+	public void delete(MasCoreSkill masCoreSkill) {
+		masCoreSkillDao.delete(masCoreSkill);
+	}
+
+	@Override
+	public MasCoreSkill find(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MasCoreSkill> findAll() {
+		// TODO Auto-generated method stub
+		return masCoreSkillDao.findAll();
+	}
+
+	@Override
+	public List<MasCoreSkill> findByCriteria(MasCoreSkill masCoreSkill) {
+		// TODO Auto-generated method stub
+		return masCoreSkillDao.findByCriteria(masCoreSkill);
+	}
+
+	@Override
+	public MasCoreSkill deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		return masCoreSkillDao.deleteById(id);
+	}
+
+}
