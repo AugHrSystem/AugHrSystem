a4a0ddc3737504d86d924bc3dcda347e35015ff0
apivakim <nuva_apiva@outlook.co.th>
2015-05-15 14:26:56 +0700

    fix stafftype

--- src/main/java/com/aug/hr/dao/impl/MasStaffTypeDaoImpl.java
+++ src/main/java/com/aug/hr/dao/impl/MasStaffTypeDaoImpl.java
@@ -2,9 +2,12 @@ package com.aug.hr.dao.impl;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Repository;
+
 import com.aug.hr.dao.MasStaffTypeDao;
 import com.aug.hr.entity.MasStaffType;
 
+@Repository
 public class MasStaffTypeDaoImpl extends GenericDaoImpl<MasStaffType, Integer> implements MasStaffTypeDao,Serializable{
 
 	/**
--- src/main/java/com/aug/hr/entity/MasStaffType.java
+++ src/main/java/com/aug/hr/entity/MasStaffType.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name="MASSTAFFTYPE")
+@Table(name="MAS_STAFFTYPE")
 public class MasStaffType extends BaseEntity{
 	
 	private Integer id;
