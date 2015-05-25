8f436aadbfbf4f1dec59f615fca384f0fa642091
Pranrajit <pranrajit@users.noreply.github.com>
2015-05-25 16:53:34 +0700

    add @Service in LeaveServiceImpl

--- src/main/java/com/aug/hr/controller/LeaveController.java
+++ src/main/java/com/aug/hr/controller/LeaveController.java
@@ -5,20 +5,22 @@
  */
 package com.aug.hr.controller;
 
-import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.aug.hr.entity.dto.AbilityDto;
+import com.aug.hr.entity.Leave;
 import com.aug.hr.services.LeaveService;
 import com.aug.hr.services.MasLeaveTypeService;
 
@@ -37,8 +39,56 @@ public class LeaveController {
 		masLeaveTypeService.findAll());
 		
 		return "/leave/leave";
+		
+		
+	}
+	
+	/*@RequestMapping(value ="/leave/listAll/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+	public @ResponseBody List<LeaveDto> listAll(@PathVariable("id") Integer id){
+		
+		return (List<LeaveDto>) leaveDtoService.searchLeave(id);
+	}
+	*/
+	
+	
+	@RequestMapping(value="/leave/add",method=RequestMethod.POST)
+	public @ResponseBody String addLeave(@RequestBody Leave leave){
+		//Hibernate.initialize(ability.getEmployee().getNameEng());
+		Hibernate.initialize(leave.getEmployee().getNameEng());
+		leaveService.create(leave);
+		return "redirect:/leave/leave";
 	}
 	
 	
+	@RequestMapping(value="/leave/findById",method=RequestMethod.POST)
+	public @ResponseBody Leave findById(@RequestParam Integer id)
+	{
+		return leaveService.findById(id);
+	}
+	
+	@RequestMapping(value="/leave/update",method=RequestMethod.POST)
+	public @ResponseBody Leave ubdateLeave(@RequestBody Leave leave){
+			leaveService.update(leave);
+			return leave;
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/leave/delete{leaveid}",method=RequestMethod.POST)
+	public @ResponseBody String deleteById(@PathVariable("leaveid") Integer leaveid){
+		
+		leaveService.deleteById(leaveid);
+		
+		return "redirect:/leave";
+	
+	
+	}
+	
+	
+	@ModelAttribute("leave")
+	Leave setupForm(){
+		return new Leave();
+	}
 	
 }
--- src/main/webapp/WEB-INF/pages/leave/leave.jsp
+++ src/main/webapp/WEB-INF/pages/leave/leave.jsp
@@ -1,10 +1,41 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Leave</title>
+<!-- Spring -->	
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+
+<!-- jQuery -->
+<script src="<c:url value="/resource/bootstrap/js/jquery-1.11.2.js" />"></script>
+
+<!-- Bootstrap -->
+<link href="<c:url value="/resource/bootstrap/css/bootstrap.css" />" rel="stylesheet" media="all">
+<link href="<c:url value="/resource/bootstrap/css/bootstrap-theme.css" />" rel="stylesheet">
+<script src="<c:url value="/resource/bootstrap/js/bootstrap.js" />"></script>
+
+<!-- Date Picker -->
+<script src="<c:url value="/resource/datepicker/js/bootstrap-datepicker.js" />"></script>
+<link href="<c:url value="/resource/datepicker/css/datepicker.css" />" rel="stylesheet" media="all">
+
+
+<!-- jQuery dataTable -->
+<script src="<c:url value="/resource/datatable/js/jquery.dataTables.js" />"></script>
+<link href="<c:url value="/resource/datatable/css/jquery.dataTables.css" />" rel="stylesheet" media="all">
+<link href="<c:url value="/resource/datatable/css/jquery.dataTables_themeroller.css" />" rel="stylesheet" media="all">
+<link href="<c:url value="/resource/datatable/css/jquery.dataTables.min.css" />" rel="stylesheet" media="all">
+
+<!-- dataTable Bootstrap -->
+<script src="<c:url value="/resource/bootstrap/js/dataTables.bootstrap.js" />"></script>
+
+<style>
+.datepicker{z-index:1151 !important;}
+
+</style>
 </head>
 <body>
 
