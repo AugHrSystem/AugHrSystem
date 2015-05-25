ce51fc75949ed4611821b96081c643cc799bb2a6
Parnell <takoyakimuffin@gmail.com>
2015-05-25 15:53:24 +0700

    add method passing data for add and edit in probation and experience entity

--- src/main/java/com/aug/hr/entity/dto/ProbationDto.java
+++ src/main/java/com/aug/hr/entity/dto/ProbationDto.java
@@ -18,9 +18,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
                 name = "searchProbation",
                 query = "select pro.id, pro.date_from, pro.date_to, pro.status,pro.employee_id, emp.employee_code from emp_probation as pro, emp_employee as emp where pro.employee_id=:empId and emp.id = pro.employee_id",
                 resultClass = ProbationDto.class),
-        @NamedNativeQuery(name = "createProbation", 
-        		query = "insert into emp_probation as pro (pro.date_from, pro.date_to, pro.status,pro.employee_id) values (?dateFrom, ?dateTo, ?status, ?employeeId)",
-        		resultClass = Probation.class)
+//        @NamedNativeQuery(name = "createProbation", 
+//        		query = "insert into emp_probation as pro (pro.date_from, pro.date_to, pro.status,pro.employee_id) values (?dateFrom, ?dateTo, ?status, ?employeeId)",
+//        		resultClass = Probation.class)
 })
 @Entity
 public class ProbationDto {
@@ -38,7 +38,7 @@ public class ProbationDto {
     @Column(name = "STATUS")
     private String status;
     @Column(name = "EMPLOYEE_ID")
-    private String employeeId;
+    private Integer employeeId;
     @Column(name = "EMPLOYEE_CODE")
     private String employeeCode;
 
@@ -82,11 +82,11 @@ public class ProbationDto {
         this.employeeCode = employeeCode;
     }
 
-    public String getEmployeeId() {
+    public Integer getEmployeeId() {
         return employeeId;
     }
 
-    public void setEmployeeId(String employeeId) {
+    public void setEmployeeId(Integer employeeId) {
         this.employeeId = employeeId;
     }
 
