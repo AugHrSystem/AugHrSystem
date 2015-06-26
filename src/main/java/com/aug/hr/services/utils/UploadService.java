4dc90959b4a29c57e12d1af490eea9cac94d7d8a
amanurat <assanai17@gmail.com>
2015-06-26 14:04:20 +0700

    reformat code.

--- src/main/java/com/aug/hr/services/utils/UploadService.java
+++ src/main/java/com/aug/hr/services/utils/UploadService.java
@@ -10,128 +10,131 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.aug.hr.constants.Constants;
+
 @Service
 public class UploadService {
-	private static final Logger log = Logger.getLogger(UploadService.class);
-	@Value("#{appProps['picture.path']}")
-	private String picpath;
-	
-	public String uploadImage(String module,String filename, MultipartFile image) throws RuntimeException, IOException {
+    private static final Logger log = Logger.getLogger(UploadService.class);
+    @Value("#{appProps['picture.path']}")
+    private String picpath;
+
+    public String uploadImage(String module, String filename, MultipartFile image) throws RuntimeException, IOException {
         try {
-            File file = new File( picpath+module+"/"+ filename);
+            File file = new File(picpath + module + "/" + filename);
             FileUtils.writeByteArrayToFile(file, image.getBytes());
             log.info("Upload:  " + file.toString() + " complete.");
         } catch (IOException e) {
-        	e.printStackTrace();
+            e.printStackTrace();
             throw e;
         }
         return Constants.SUCCESS;
     }
-	
-	public String deleteImage(String module,String filename) throws RuntimeException, IOException {
+
+    public String deleteImage(String module, String filename) throws RuntimeException, IOException {
         System.out.println("Expenses Delete Image");
-		try {
-				String sDestPath = picpath+module+"/"+ filename; 
-				File fDelFile = new File( picpath+module+"/"+ filename);
-				System.out.println("Destination to Delete a image :"+sDestPath);
-				fDelFile.delete();
-				System.out.println("Image is Delete");
-			} catch (Exception e) {
+        try {
+            String sDestPath = picpath + module + "/" + filename;
+            File fDelFile = new File(picpath + module + "/" + filename);
+            System.out.println("Destination to Delete a image :" + sDestPath);
+            fDelFile.delete();
+            System.out.println("Image is Delete");
+        } catch (Exception e) {
 
-				e.printStackTrace();
-				return e.getMessage();
+            e.printStackTrace();
+            return e.getMessage();
 
-			}
+        }
         return Constants.SUCCESS;
     }
-	
-	public static String uploadFile(String sSourcePath, String sDestFileName, String sAppRootPath,String sFolderPath){
-		try {
-			String sDestPath = sAppRootPath + sFolderPath + sDestFileName; 
-			
-			File fSourceFile = new File(sSourcePath);
-			File fDestFile = new File(sDestPath);
-			log.info("Source :"+sSourcePath);
-			log.info("Destination :"+sDestPath);
-			FileUtils.copyFile(fSourceFile, fDestFile);
 
-			} catch (Exception e) {
+    public static String uploadFile(String sSourcePath, String sDestFileName, String sAppRootPath, String sFolderPath) {
+        try {
+            String sDestPath = sAppRootPath + sFolderPath + sDestFileName;
 
-				e.printStackTrace();
-				return e.getMessage();
+            File fSourceFile = new File(sSourcePath);
+            File fDestFile = new File(sDestPath);
+            log.info("Source :" + sSourcePath);
+            log.info("Destination :" + sDestPath);
+            FileUtils.copyFile(fSourceFile, fDestFile);
 
-			}
-			return Constants.SUCCESS;
-	}
-	
-	public static String uploadZipFile(String sSourcePath, String sAppRootPath, String sContextPath, String sFolderPath){
-		try {
-			String sDestPath = sAppRootPath + sContextPath + sFolderPath ; 
-			
-			File fSourceFile = new File(sSourcePath);
-			File fDestFile = new File(sDestPath);
-			log.info("Source :"+sSourcePath);
-			log.info("Destination :"+sDestPath);
-			FileUtils.copyFile(fSourceFile, fDestFile);
-			log.info("UPLOAD ZIP FILE SUC");
-			} catch (Exception e) {
+        } catch (Exception e) {
 
-				e.printStackTrace();
-				return e.getMessage();
+            e.printStackTrace();
+            return e.getMessage();
 
-			}
-			return Constants.SUCCESS;
-	}
-	public static String deleteFile(String sDestFileName, String sAppRootPath, String sContextPath, String sFolderPath){
-		System.out.println("Expenses Delete File");
-		try {
-				String sDestPath = sAppRootPath + sContextPath + sFolderPath + sDestFileName; 
-				File fDelFile = new File(sDestPath);
-				System.out.println("Destination to Delete a file :"+sDestPath);
-				fDelFile.delete();
-				System.out.println("File is Delete");
-			} catch (Exception e) {
+        }
+        return Constants.SUCCESS;
+    }
 
-				e.printStackTrace();
-				return e.getMessage();
+    public static String uploadZipFile(String sSourcePath, String sAppRootPath, String sContextPath, String sFolderPath) {
+        try {
+            String sDestPath = sAppRootPath + sContextPath + sFolderPath;
 
-			}
-			return Constants.SUCCESS;
-	}
-	public static String deleteFolderPath(String sAppRootPath, String sFolderPath){
-		System.out.println("Expenses Delete File");
-		try {
-				String sFolderFullPath = sAppRootPath +  sFolderPath; 
-				File fDelFile = new File(sFolderFullPath);
-				System.out.println("Destination to Delete a Folder :"+sFolderFullPath);
-				fDelFile.delete();
-				System.out.println("Folder is Delete");
-			} catch (Exception e) {
+            File fSourceFile = new File(sSourcePath);
+            File fDestFile = new File(sDestPath);
+            log.info("Source :" + sSourcePath);
+            log.info("Destination :" + sDestPath);
+            FileUtils.copyFile(fSourceFile, fDestFile);
+            log.info("UPLOAD ZIP FILE SUC");
+        } catch (Exception e) {
 
-				e.printStackTrace();
-				return e.getMessage();
+            e.printStackTrace();
+            return e.getMessage();
 
-			}
-			return Constants.SUCCESS;
-	}
-	
-	public static String copyFileForClientSubmission(String sDestFileName, String sAppRootPath, String sContextPath, String sCopiedPath, String sFolderPath){
-		try {
-			String sDestPath = sAppRootPath + sContextPath + sFolderPath + sDestFileName; 
-			String sCopyPath = sAppRootPath + sContextPath +sCopiedPath +sDestFileName;
-			File fSourceFile = new File(sCopyPath);
-			File fDestFile = new File(sDestPath);
-			System.out.println("Source (copy) :"+sCopyPath);
-			System.out.println("Destination :"+sDestPath);
-			FileUtils.copyFile(fSourceFile, fDestFile);
+        }
+        return Constants.SUCCESS;
+    }
 
-			} catch (Exception e) {
+    public static String deleteFile(String sDestFileName, String sAppRootPath, String sContextPath, String sFolderPath) {
+        System.out.println("Expenses Delete File");
+        try {
+            String sDestPath = sAppRootPath + sContextPath + sFolderPath + sDestFileName;
+            File fDelFile = new File(sDestPath);
+            System.out.println("Destination to Delete a file :" + sDestPath);
+            fDelFile.delete();
+            System.out.println("File is Delete");
+        } catch (Exception e) {
 
-				e.printStackTrace();
-				return e.getMessage();
+            e.printStackTrace();
+            return e.getMessage();
 
-			}
-			return Constants.SUCCESS;
-	}
+        }
+        return Constants.SUCCESS;
+    }
+
+    public static String deleteFolderPath(String sAppRootPath, String sFolderPath) {
+        System.out.println("Expenses Delete File");
+        try {
+            String sFolderFullPath = sAppRootPath + sFolderPath;
+            File fDelFile = new File(sFolderFullPath);
+            System.out.println("Destination to Delete a Folder :" + sFolderFullPath);
+            fDelFile.delete();
+            System.out.println("Folder is Delete");
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return e.getMessage();
+
+        }
+        return Constants.SUCCESS;
+    }
+
+    public static String copyFileForClientSubmission(String sDestFileName, String sAppRootPath, String sContextPath, String sCopiedPath, String sFolderPath) {
+        try {
+            String sDestPath = sAppRootPath + sContextPath + sFolderPath + sDestFileName;
+            String sCopyPath = sAppRootPath + sContextPath + sCopiedPath + sDestFileName;
+            File fSourceFile = new File(sCopyPath);
+            File fDestFile = new File(sDestPath);
+            System.out.println("Source (copy) :" + sCopyPath);
+            System.out.println("Destination :" + sDestPath);
+            FileUtils.copyFile(fSourceFile, fDestFile);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return e.getMessage();
+
+        }
+        return Constants.SUCCESS;
+    }
 }
 
--- src/main/java/com/aug/servlet/DisplayImageServlet.java
+++ src/main/java/com/aug/servlet/DisplayImageServlet.java
@@ -27,11 +27,11 @@ import org.springframework.util.StringUtils;
 import com.aug.hr.constants.Constants;
 
 
-
 public class DisplayImageServlet extends HttpServlet {
-	
-	protected static Logger logger = Logger.getLogger(DisplayImageServlet.class);
-	private static final long serialVersionUID = 8500706756578949263L;
+
+    protected static Logger logger = Logger.getLogger(DisplayImageServlet.class);
+    private static final long serialVersionUID = 8500706756578949263L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -39,22 +39,21 @@ public class DisplayImageServlet extends HttpServlet {
         super();
     }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (!StringUtils.isEmpty(request.getParameter("namespace"))){
-			this.displayImage(request, response);
-		}
-		else this.previewImagePath(request, response);
-	}
-	
-	public void displayImage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		ResourceBundle resource = ResourceBundle.getBundle("ApplicationResources");
-		File imageFile = new File(resource.getString("picture.path")+request.getParameter("namespace")+"/"+request.getParameter("fileName"));
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!StringUtils.isEmpty(request.getParameter("namespace"))) {
+            this.displayImage(request, response);
+        } else this.previewImagePath(request, response);
+    }
+
+    public void displayImage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ResourceBundle resource = ResourceBundle.getBundle("ApplicationResources");
+        File imageFile = new File(resource.getString("picture.path") + request.getParameter("namespace") + "/" + request.getParameter("fileName"));
         FileInputStream fin = new FileInputStream(imageFile);
         ServletOutputStream outStream = response.getOutputStream();
         response.setContentType("image/jpeg");
@@ -64,92 +63,92 @@ public class DisplayImageServlet extends HttpServlet {
             outStream.write(i);
         }
         fin.close();
-	}
-	
-	public void previewImagePath(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		String name = null;
-		String size = null;
-		String path = null;		
-		String pathName = null;
-		InputStream is = null;
-		OutputStream sos = null;
-		BufferedImage bufferedImage = null;
-		BufferedInputStream bis = null;
-		ResourceBundle resource = null;
-		byte[] image = null;
-		byte[] tempbyte = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		int width = 1000;
-		int calcHeight = 0;
-		
-		try{
-			name = (String)request.getParameter("name");
-			size = (String)request.getParameter("size");
-			resource = ResourceBundle.getBundle("ApplicationResources");
-			path = changeFileSeparatorForOS(resource.getString("ApplicationAbsolutePath")+resource.getString("UserAttachments"));
-			pathName = path + name;
-					
-			logger.info(" ----------------- path -------------- :"+pathName);
-			if(name !=null && !"".equals(name)){
-				File imageFile = new File(pathName);
-				if(!imageFile.exists()){
-					pathName = path+Constants.DEFAULT_IMAGE_PRODUCT;
-					imageFile = new File(pathName);
-				}
-				
-				image = new byte[16];
-				sos = response.getOutputStream();
-				
-				if("mini".equals(size)){
-					 bufferedImage = ImageIO.read(imageFile); 
-		             calcHeight =(width * bufferedImage.getHeight() / bufferedImage.getWidth());  
-		             ImageIO.write(createResizedCopy(bufferedImage, width, calcHeight), "JPG", response.getOutputStream());  		
-				}else{				
-					is = new FileInputStream(imageFile);
-					bis = new BufferedInputStream(is);
-					int count = 0;
-					while((count = bis.read(image)) >= 0){
-						sos.write(image, 0, count);
-					}
-					is.close();
-					bis.close();
-				}
-			}
-	
-		}catch(NullPointerException ex){
-			sos.write(tempbyte);
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			if(sos != null){
-				sos.flush();
-				sos.close();
-			}
-			name = null;
-			size = null;
-			resource = null;
-			path = null;		
-			pathName = null;
-			is = null;
-			sos = null;
-			bufferedImage = null;
-			bis = null;
-			image = null;
-			tempbyte = null;
-		}
-	}
-	
-	BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight) {  
-	     BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_RGB);  
-	     Graphics2D g = scaledBI.createGraphics();  
-	     g.setComposite(AlphaComposite.Src);  
-	     g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);  
-	     g.dispose();  
-	     return scaledBI;  
-	}  
-	 
-	private static String changeFileSeparatorForOS(final String pathname) {
-		return pathname.replaceAll("(/|\\|/|\\\\)", Matcher.quoteReplacement(File.separator));
-	}
+    }
+
+    public void previewImagePath(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = null;
+        String size = null;
+        String path = null;
+        String pathName = null;
+        InputStream is = null;
+        OutputStream sos = null;
+        BufferedImage bufferedImage = null;
+        BufferedInputStream bis = null;
+        ResourceBundle resource = null;
+        byte[] image = null;
+        byte[] tempByte = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int width = 1000;
+        int calcHeight = 0;
+
+        try {
+            name = (String) request.getParameter("name");
+            size = (String) request.getParameter("size");
+            resource = ResourceBundle.getBundle("ApplicationResources");
+            path = changeFileSeparatorForOS(resource.getString("ApplicationAbsolutePath") + resource.getString("UserAttachments"));
+            pathName = path + name;
+
+            logger.info(" ----------------- path -------------- :" + pathName);
+            if (name != null && !"".equals(name)) {
+                File imageFile = new File(pathName);
+                if (!imageFile.exists()) {
+                    pathName = path + Constants.DEFAULT_IMAGE_PRODUCT;
+                    imageFile = new File(pathName);
+                }
+
+                image = new byte[16];
+                sos = response.getOutputStream();
+
+                if ("mini".equals(size)) {
+                    bufferedImage = ImageIO.read(imageFile);
+                    calcHeight = (width * bufferedImage.getHeight() / bufferedImage.getWidth());
+                    ImageIO.write(createResizedCopy(bufferedImage, width, calcHeight), "JPG", response.getOutputStream());
+                } else {
+                    is = new FileInputStream(imageFile);
+                    bis = new BufferedInputStream(is);
+                    int count = 0;
+                    while ((count = bis.read(image)) >= 0) {
+                        sos.write(image, 0, count);
+                    }
+                    is.close();
+                    bis.close();
+                }
+            }
+
+        } catch (NullPointerException ex) {
+            sos.write(tempByte);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sos != null) {
+                sos.flush();
+                sos.close();
+            }
+            name = null;
+            size = null;
+            resource = null;
+            path = null;
+            pathName = null;
+            is = null;
+            sos = null;
+            bufferedImage = null;
+            bis = null;
+            image = null;
+            tempByte = null;
+        }
+    }
+
+    BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight) {
+        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = scaledBI.createGraphics();
+        g.setComposite(AlphaComposite.Src);
+        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
+        g.dispose();
+        return scaledBI;
+    }
+
+    private static String changeFileSeparatorForOS(final String pathname) {
+        return pathname.replaceAll("(/|\\|/|\\\\)", Matcher.quoteReplacement(File.separator));
+    }
 
 }
 
